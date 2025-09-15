9c5462cc1238491acd456d34cca42ea60ddb81d8
Tristan-railtown <tristan@railtown.ai>
2025-09-15 16:08:02 -0700

    commit

--- packages/railtracks/tests/unit_tests/built_nodes/easy_usage_wrappers/test_agent.py
+++ packages/railtracks/tests/unit_tests/built_nodes/easy_usage_wrappers/test_agent.py
@@ -82,13 +82,3 @@ def test_agent_node_with_manifest(mock_tool_node, mock_llm, mock_manifest, mock_
     )
     assert isinstance(node_cls, type)
     assert node_cls.name() == "AgentWithManifest"
-
-def test_agent_node_tool_nodes_func(mock_llm, mock_function, mock_sys_mes):
-    node_cls = agent_node(
-        name="AgentWithFuncTool",
-        tool_nodes=[mock_function],
-        llm=mock_llm,
-        system_message=mock_sys_mes
-    )
-    assert isinstance(node_cls, type)
-    assert node_cls.name() == "AgentWithFuncTool"
