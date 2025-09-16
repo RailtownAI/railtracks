b9bc2b132375f13b44d76f8db33e0a9220ac5378
somtirtha <somtirtha.roy@gmail.com>
2025-09-16 11:22:46 -0700

    Add ruff linting

--- packages/railtracks/src/railtracks/llm/models/__init__.py
+++ packages/railtracks/src/railtracks/llm/models/__init__.py
@@ -2,4 +2,12 @@ from .api_providers import AnthropicLLM, GeminiLLM, HuggingFaceLLM, OpenAILLM, C
 from .cloud.azureai import AzureAILLM
 from .local.ollama import OllamaLLM
 
-__all__ = [OpenAILLM, AnthropicLLM, GeminiLLM, AzureAILLM, OllamaLLM, HuggingFaceLLM, CohereLLM]
+__all__ = [
+    OpenAILLM,
+    AnthropicLLM,
+    GeminiLLM,
+    AzureAILLM,
+    OllamaLLM,
+    HuggingFaceLLM,
+    CohereLLM,
+]
