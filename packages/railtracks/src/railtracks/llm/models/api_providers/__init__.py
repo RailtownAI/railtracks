ff27e53731192c004e830c8dd45e938009e48922
somtirtha <somtirtha.roy@gmail.com>
2025-09-16 11:18:09 -0700

    Add CohereLLM to package for export

--- packages/railtracks/src/railtracks/llm/models/api_providers/__init__.py
+++ packages/railtracks/src/railtracks/llm/models/api_providers/__init__.py
@@ -1,6 +1,7 @@
 from .anthropic import AnthropicLLM
 from .gemini import GeminiLLM
 from .huggingface import HuggingFaceLLM
+from .cohere import CohereLLM
 from .openai import OpenAILLM
 
-__all__ = [OpenAILLM, AnthropicLLM, GeminiLLM, HuggingFaceLLM]
+__all__ = [OpenAILLM, AnthropicLLM, GeminiLLM, HuggingFaceLLM, CohereLLM]
