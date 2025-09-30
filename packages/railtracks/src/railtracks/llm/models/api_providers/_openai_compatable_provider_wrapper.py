4c176d86f12e6e516414c1dcb7d5e4d98cd49f74
Logan <logan@railtown.ai>
2025-09-30 15:23:28 -0700

    bypassed logic in the validation steps for openai compatable models

--- packages/railtracks/src/railtracks/llm/models/api_providers/_openai_compatable_provider_wrapper.py
+++ packages/railtracks/src/railtracks/llm/models/api_providers/_openai_compatable_provider_wrapper.py
@@ -9,3 +9,11 @@ class OpenAICompatibleProviderWrapper(ProviderLLMWrapper, ABC):
 
     def full_model_name(self, model_name: str) -> str:
         return f"openai/{self.model_type().lower()}/{model_name}"
+    
+    def _pre_init_provider_check(self, model_name: str):
+        # For OpenAI compatible providers, we skip the provider check since there is no way to do it.
+        return model_name
+    
+    def _validate_tool_calling_support(self):
+        # For OpenAI compatible providers, we skip the tool calling support check since there is no way to do it.
+        return
