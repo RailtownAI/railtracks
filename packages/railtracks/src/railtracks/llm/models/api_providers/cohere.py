ad3253738401c2766268914453bbe0aa38789577
Tristan-railtown <tristan@railtown.ai>
2025-10-01 14:59:11 -0700

    test

--- packages/railtracks/src/railtracks/llm/models/api_providers/cohere.py
+++ packages/railtracks/src/railtracks/llm/models/api_providers/cohere.py
@@ -8,5 +8,5 @@ class CohereLLM(ProviderLLMWrapper):
     """
 
     @classmethod
-    def model_type(cls):
-        return ModelProvider.COHERE
+    def model_type(cls) -> str:
+        return "cohere_chat"
