8c4fbc5f831caf41811e1ec066d75a12d62153be
Tristan-railtown <tristan@railtown.ai>
2025-10-01 13:34:25 -0700

    cohere

--- packages/railtracks/src/railtracks/llm/models/api_providers/cohere.py
+++ packages/railtracks/src/railtracks/llm/models/api_providers/cohere.py
@@ -1,3 +1,4 @@
+from ..providers import ModelProvider
 from ._provider_wrapper import ProviderLLMWrapper
 
 
@@ -7,5 +8,5 @@ class CohereLLM(ProviderLLMWrapper):
     """
 
     @classmethod
-    def model_type(cls) -> str:
-        return "cohere_chat"
+    def model_type(cls):
+        return ModelProvider.HUGGINGFACE
