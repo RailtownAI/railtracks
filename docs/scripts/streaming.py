7bc2e2e7ca5076a3bba8ec752a508c2bf482f938
Logan <logan@railtown.ai>
2025-10-07 12:57:52 -0700

    updated docs scripts

--- docs/scripts/streaming.py
+++ docs/scripts/streaming.py
@@ -1,8 +1,12 @@
 # --8<-- [start: streaming_flag]
-import railtracks.llm as llm 
+from typing import Generator
+from railtracks.built_nodes.concrete.response import LLMResponse
+import railtracks.llm as llm
+from railtracks.llm.response import Response 
 
 model = llm.OpenAILLM(model_name="gpt-4o", stream=True)
 # --8<-- [end: streaming_flag]
+
 
 # --8<-- [start: streaming_usage]
 model = llm.OpenAILLM(model_name="gpt-4o", stream=True)
@@ -11,9 +15,10 @@ response = model.chat(llm.MessageHistory([
     llm.UserMessage("Tell me who you are are"),
 ]))
 
-# The response object can act as an iterator returning string chunks terminating with the complete message. 
-for chunk in response:
-    print(chunk)
+# The response object can act as an iterator returning string chunks terminating with the complete message.
+if isinstance(response, Generator):
+    for chunk in response:
+        print(chunk)
 # --8<-- [end: streaming_usage]
 
 # --8<-- [start: streaming_with_agents]
@@ -24,6 +29,7 @@ agent = rt.agent_node(
 )
 
 # --8<-- [end: streaming_with_agents]
+
 
 # --8<-- [start: streaming_agent_usage]
 agent = rt.agent_node(
@@ -36,9 +42,10 @@ async def main():
         rt.llm.UserMessage("Tell me who you are are"),
         ]))
     
-    # The response object can act as an iterator returning string chunks terminating with the complete message. 
-    for chunk in result:
-        print(chunk)
+    # The response object can act as an iterator returning string chunks terminating with the complete message.
+    if isinstance(result, Generator): 
+        for chunk in result:
+            print(chunk)
     
     
 
