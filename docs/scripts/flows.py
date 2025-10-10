27f78777739d63ca0789646ebe7c76baa2e1eb52
Logan <logan@railtown.ai>
2025-10-10 13:59:33 -0700

    fixed typing

--- docs/scripts/flows.py
+++ docs/scripts/flows.py
@@ -126,24 +126,28 @@ class StructuredResponse(BaseModel):
 QualityAssuranceAgent = rt.agent_node(
     name="Quality Assurance Agent",
     output_schema=StructuredResponse,
+    llm=rt.llm.OpenAILLM("gpt-4o"),
     #adding all other arguments as needed
     )
 
 ProductExpertAgent = rt.agent_node(
     name="Product Expert Agent",
     output_schema=StructuredResponse,
+    llm=rt.llm.OpenAILLM("gpt-4o"),
     #adding all other arguments as needed
     )
 
 BillingAgent = rt.agent_node(
     name="Billing Agent",
     output_schema=StructuredResponse,
+    llm=rt.llm.OpenAILLM("gpt-4o"),
     #adding all other arguments as needed
     )
     
 TechnicalAgent = rt.agent_node(
     name="Technical Support Agent",
     output_schema=StructuredResponse,
+    llm=rt.llm.OpenAILLM("gpt-4o"),
     #adding all other arguments as needed
     )
 
