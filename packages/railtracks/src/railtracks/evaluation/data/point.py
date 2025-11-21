c5e31693a90c88e255cd3566ec8336d3b05a13a5
Amir Refaee <amir@railtown.ai>
2025-11-21 11:04:40 -0800

    updated point metadata

--- packages/railtracks/src/railtracks/evaluation/data/point.py
+++ packages/railtracks/src/railtracks/evaluation/data/point.py
@@ -4,6 +4,6 @@ from uuid import UUID, uuid4
 class DataPoint(BaseModel):
     """A class representing a single data point"""
     agent_input: str
-    agent_output: str
-    expected_output: str | None = None
+    agent_output: str | BaseModel
+    expected_output: str | BaseModel | None = None
     _id: UUID = Field(default_factory=uuid4)
