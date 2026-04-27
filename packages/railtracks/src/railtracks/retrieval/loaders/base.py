a4aa952130825a7818de8506140076bd8454ae21
Amir Refaee <amir@railtown.ai>
2026-04-27 16:29:33 -0700

    conftest

--- packages/railtracks/src/railtracks/retrieval/loaders/base.py
+++ packages/railtracks/src/railtracks/retrieval/loaders/base.py
@@ -19,17 +19,6 @@ class BaseDocumentLoader(ABC):
     full corpus to load. This enables true streaming behaviour where a document
     is chunked and embedded while subsequent documents are still being loaded.
 
-    Example:
-        ::
-
-            class MyLoader(BaseDocumentLoader):
-                async def astream(self) -> AsyncGenerator[Document, None]:
-                    for item in my_source:
-                        yield Document(content=item.text, source=item.url)
-
-            loader = MyLoader()
-            async for doc in loader.astream():
-                print(doc.content)
     """
 
     @abstractmethod
@@ -44,7 +33,7 @@ class BaseDocumentLoader(ABC):
         Yields:
             Document: The next available document from the source.
         """
-        yield # This needs to be here to make this an async generator, read: https://discuss.python.org/t/overloads-of-async-generators-inconsistent-coroutine-wrapping/56665
+        yield # type: ignore This needs to be here to make this an async generator, read: https://discuss.python.org/t/overloads-of-async-generators-inconsistent-coroutine-wrapping/56665
 
     async def aload(self) -> list[Document]:
         """Load all documents and return them as a list.
