1c88c4be5f34c13c874e3ed9210a5620d96b2ecc
Pooria Ashrafian <pooria@railtown.ai>
2026-06-08 14:01:09 -0700

    feat: include ChromaCloudBackend in exports

--- packages/railtracks/src/railtracks/retrieval/stores/__init__.py
+++ packages/railtracks/src/railtracks/retrieval/stores/__init__.py
@@ -7,11 +7,12 @@ from .models import (
 )
 from .protocol import Store
 from .vector import VectorStore
-from .vector.backends import ChromaBackend, DistanceMetric, PgvectorBackend
+from .vector.backends import ChromaBackend, DistanceMetric, PgvectorBackend, ChromaCloudBackend
 from .vector.backends import InMemoryBackend as InMemoryVectorBackend
 
 __all__ = [
     "ChromaBackend",
+    "ChromaCloudBackend",
     "DistanceMetric",
     "Entity",
     "InMemoryVectorBackend",
